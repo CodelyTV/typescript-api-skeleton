14077635687862f626446edfe17fb387dd9ac4e9
Javier Ferrer <javier.mailserio@gmail.com>
2023-08-04 17:58:35 +0200

    feat: update dependencies

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const server = app.listen(app.get("port"), () => {
 	console.log(
 		"  App is running at http://localhost:%d in %s mode",
 		app.get("port"),
-		app.get("env")
+		app.get("env"),
 	);
 	console.log("  Press CTRL-C to stop\n");
 });
